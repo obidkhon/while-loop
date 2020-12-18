8e63acd78ba34570e08a5343616815b289212f90
obidkhon <obid.sharipov.80@mail.ru>
2020-12-18 21:44:55 +0500

    while

---
+++ src/MylLoop.java
@@ -0,0 +1,11 @@
+public class MylLoop {
+    public static void main(String[] args) {
+        int shoftoli=1;
+        while (shoftoli<=15){
+            System.out.println(shoftoli+ " inchi shoftoli");
+            shoftoli++;
+           shoftoli= shoftoli+5;
+        }
+
+    }
+}
